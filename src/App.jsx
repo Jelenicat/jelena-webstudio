f102e4bc69ece2259db13a2fcc625b8015782b96
Jelena Tanasković <jelenatanaskovicj@gmail.com>
2026-01-23 16:49:52 +0100

    l

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import AdminLogin from "./pages/AdminLogin";
 import Admin from "./pages/Admin";
 
 function App() {
+  const isAdmin = localStorage.getItem("isAdmin") === "true";
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin-login" element={<AdminLogin />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route
+          path="/admin"
+          element={isAdmin ? <Admin /> : <Navigate to="/admin-login" />}
+        />
       </Routes>
     </BrowserRouter>
   );
